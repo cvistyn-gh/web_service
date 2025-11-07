a8a41f1f223922211b6ba10124132173a938c42c
cvistyn-gh <cvistyn87@gmail.com>
2025-11-07 12:17:16 +0500

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -8,3 +8,12 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=5000)
+
+from flask import render_template
+
+@app.route("/data_to")
+def data_to():
+    some_pars = {'user':'Ivan','color':'red'}
+    some_str = 'Hello my dear friends!'
+    some_value = 10
+    return render_template('simple.html',some_str=some_str,some_value=some_value,some_pars=some_pars)
