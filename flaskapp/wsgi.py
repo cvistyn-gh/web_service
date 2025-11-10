0394806c61baa642f187f0d1ed386451fc481feb
cvistyn-gh <cvistyn87@gmail.com>
2025-11-10 16:14:20 +0500

    repair wsgi.py

--- flaskapp/wsgi.py
+++ flaskapp/wsgi.py
@@ -1,10 +1,10 @@
-# from some_app import app
+from some_app import app
 
-# if __name__ == "__main__":
-    # app.run()
-import sys
-path = '/home/cvistyn/web_service'
-if path not in sys.path:
-    sys.path.append(path)
+if __name__ == "__main__":
+    app.run()
+# import sys
+# path = '/home/cvistyn/web_service'
+# if path not in sys.path:
+    # sys.path.append(path)
 
-from flaskapp.some_app import app as application
+# from flaskapp.some_app import app as application
